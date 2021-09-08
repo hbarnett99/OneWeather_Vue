a39a3db9f149fe07b438d066f9ae562014586f75
Zoe Parkinson <zpar0002@student.monash.edu>
2021-09-08 20:05:14 +1000

    started location by search feature

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="one weather logo" src="./assets/ow_logo.png">
+  <HelloWorld/>
 </template>
 
 <script>
