45618516d40e6aae0af75f6ffeb73403ebbe9683
jaiclapp <jcla0016@student.monash.edu>
2021-09-08 16:37:36 +1000

    WIP - Testing Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to Your Vue.js App</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 };
 </script>
