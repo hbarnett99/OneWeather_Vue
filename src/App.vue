4e861bd825c6cd3c564237ed65aad6ba20e2a57f
Nick Wang <hwan0109@student.monash.edu>
2021-09-10 18:49:48 +1000

    add to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HorizonStars/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HorizonStars from './components/HorizonStars.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HorizonStars
   }
 };
 </script>
