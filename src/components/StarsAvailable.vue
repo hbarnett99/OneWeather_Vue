628765fb85b7479ff61749da4564998c3534ecb0
Nick Wang <hwan0109@student.monash.edu>
2021-09-21 20:28:33 +1000

    add more stars

--- src/components/StarsAvailable.vue
+++ src/components/StarsAvailable.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <h1>Stars</h1>
-    <table :key="star.name + star.designations ?? ''" v-for="star in stars">
-      <td >
-        <Star :star="star" />
-        <Button text="Add" :id="star.name + star.designations ?? ''" @btn-click="onClick" />
-      </td>
-    </table>
+    <div style="height:200px;overflow:auto">
+      <table :key="star.name + star.designations ?? ''" v-for="star in stars">
+        <td >
+          <Star :star="star" />
+          <Button text="Add" :id="star.name + star.designations ?? ''" @btn-click="onClick" />
+        </td>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 import Star from './Star.vue';
 import Button from "./Button.vue";
+
+const STELLAR_URL = "http://localhost:8090/api/objects";
 
 /**
  * Fetch a stellar object
@@ -21,7 +25,7 @@ import Button from "./Button.vue";
  * @returns Information of stellar object
  */
 async function fetchStellar(name) {
-  return await fetch(`http://localhost:8090/api/objects/info?format=json&name=${name}`);
+  return await fetch(`${STELLAR_URL}/info?format=json&name=${name}`);
 }
 
 export default {
@@ -32,13 +36,15 @@ export default {
     };
   },
   async created() {
-    const resPlanets = await fetch("http://localhost:8090/api/objects/listobjectsbytype?type=SolarSystem:planet");
+    // Fetch names of stellar objects
+    const resPlanets = await fetch(`${STELLAR_URL}/listobjectsbytype?type=SolarSystem:planet`);
     const planets = await resPlanets.json();
-    const resStars = await fetch("http://localhost:8090/api/objects/listobjectsbytype?type=NebulaMgr:4");
+    const resStars = await fetch(`${STELLAR_URL}/listobjectsbytype?type=StarMgr`);
     const stars = await resStars.json();
 
     const objs = [].concat(planets, stars);
 
+    // Fetch information for all stellar objects
     for (const s of objs) {
       const res = await fetchStellar(s);
       const star = await res.json();
