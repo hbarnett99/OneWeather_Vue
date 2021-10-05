8dfef7057cd4e8de30e3401817e83f3e8b09ef60
Nick Wang <hwan0109@student.monash.edu>
2021-10-05 20:22:52 +1100

    add calculations to determine star visibility based on weather

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,17 +1,59 @@
 <template>
   <span>
-    Name: {{star.name || star.designations}}, {{star["above-horizon"] ? "Above" : "Below"}} Horizon
+    Name: {{ star.name || star.designations }}, Visibility ({{ calculateVisibility() }} %)
   </span>
 </template>
 
 <script>
 export default {
   props: {
-    star: Object
-  }
+    star: Object,
+    weather: Object,
+  },
+  methods: {
+    calculateVisibility: function () {
+      const { ambientLum, illumination, aboveHorizon } = this.star;
+
+      // Cannot see star if it is below horizon
+      if (!aboveHorizon) {
+        return 0;
+      }
+
+      return ambientLum * illumination * this.calculateBlockage();
+    },
+
+    calculateBlockage: function() {
+      const isDay = !!this.weather.is_day;
+
+      // Assume we can't see stars during day
+      if (isDay) {
+        return 0;
+      }
+
+      const {
+        will_it_rain,
+        chance_of_rain,
+        chance_of_snow,
+        cloud
+      } = this.weather.condition;
+
+      // Chance of it raining or snowing
+      const
+        snowProb = chance_of_snow/100,
+        rainProb = chance_of_rain/100,
+        noSnowProb = 1 - snowProb,
+        noRainProb = 1 - rainProb,
+        rainSnowProb = snowProb * noRainProb + rainProb * noSnowProb,
+        chanceOfVisibility = 1 - (will_it_rain || rainSnowProb);
+
+      // Blocking view due to clouds
+      const cloudBlock = cloud > 25 ? 1 : cloud/25;
+
+      return cloudBlock * chanceOfVisibility;
+    }
+  },
 };
 </script>
 
 <style>
-
 </style>
