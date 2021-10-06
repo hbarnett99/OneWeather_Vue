bbd53d3803ff22b081c91a63688846f63613709e
Zoe Parkinson <zpar0002@student.monash.edu>
2021-10-07 00:08:23 +1100

    added graph of previous mars temperature

---
+++ src/components/MarsChart.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="example">
+    <apexcharts height="350" :options="chartOptions" :series="series"></apexcharts>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: 'MarsChart',
+  components: {
+    apexcharts: VueApexCharts,
+  },
+  props: {
+    dates: Array,
+    mintemp: Array,
+    maxtemp: Array
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          id: 'barChart',
+          type: 'line'
+        },
+        xaxis: {
+          categories: this.dates,
+          text: 'Date'
+        },
+        yaxis: [{
+          title: {
+            text: 'Temperature'
+          }
+        }], 
+        dataLabels: {
+          enabled: true,
+        },
+        title: {
+          text: 'Previous temperature'
+        }
+      },
+      series: [{
+        name: 'Minimum Temperature(°C)',
+        type: 'line',
+        data: this.mintemp
+      },{
+        name: 'Maximum Temperautre (°C)',
+        type: 'line',
+        data: this.maxtemp
+      }]
+    };
+  },
+  watch: {
+    dates() {
+      this.updateChart();
+    },
+    mintemp() {
+      this.series[0].data = this.mintemp;
+    },
+    maxtemp() {
+      this.series[2].data = this.maxtemp;
+
+    }
+  },
+  methods: {
+    //Update the value of x-axis (default: 1, 2, 3 ...) to dates
+    updateChart() {
+      this.chartOptions = {
+        xaxis: {
+          categories: this.dates
+        }
+      };
+    }
+  }
+};
+</script>
--- src/components/MarsWeather.vue
+++ src/components/MarsWeather.vue
@@ -6,7 +6,7 @@
        <p> Maximum Temperature: {{ latestMaxTemp }}&#176; C </p>
     <br>
     <br>
-    <h3> Past 7 days </h3>
+    <h3> Past 6 days </h3>
       <div style="height:200px;overflow:auto">
     <p> Sol: {{previousSols[0]}} </p>
     <p> Date: {{previousDays[0]}} </p>
@@ -38,11 +38,26 @@
     <p> Minimum temperature: {{previousMinTemp[5]}}&#176; C </p>
     <p> Maximum temperature: {{previousMaxTemp[5]}}&#176; C </p>
     </div>
+
+    <br>
+    <p> Weekly average minimum temperature: {{averageMin}}&#176; C </p>
+    <p> Weekly average maximum temperature: {{averageMax}}&#176; C </p>
+
+    <div class="card-body" style="position: relative;">
+      <MarsChart :dates="previousDays" :mintemp="previousMinTemp" :maxtemp="previousMaxTemp"/>
+    </div>
+
 </template>
 
 <script>
 import axios from "axios";
+import MarsChart from '../components/MarsChart.vue';
 export default {
+  name: "Mars",
+
+  components: {
+    MarsChart
+  },
   data() {
     return {
       latestSol: "",
@@ -53,6 +68,8 @@ export default {
       previousDays: [],
       previousMinTemp: [],
       previousMaxTemp: [],
+      averageMin: "",
+      averageMax: ""
     };
   },
   created(){
@@ -67,6 +84,7 @@ export default {
       // const response = await axios.get(
       //   `https://api.maas2.apollorion.com/${sol}`
       // );
+      // can get sunrise and sunset
       this.latestSol = response.data.sol;
       this.latestDate = response.data.terrestrial_date.substring(0,10);
       this.latestMinTemp = response.data.min_temp;
@@ -82,9 +100,24 @@ export default {
         );
         this.previousDays.push(response.data.terrestrial_date.substring(0,10));
         this.previousMinTemp.push(response.data.min_temp);
-        this.previousMaxTemp.push(response.data.max_temp); 
+        this.previousMaxTemp.push(response.data.max_temp);  
       }
+      this.getAverage();
+    },
+    getAverage(){
+      this.averageMin = this.latestMinTemp;
+      this.averageMax = this.latestMaxTemp;
+      for (let i=0; i < 6;  i++){
+        this.averageMin =  this.averageMin + this.previousMinTemp[i];
+        this.averageMax =  this.averageMax + this.previousMaxTemp[i];
+      }
+      this.averageMin = (this.averageMin/7).toFixed(2);
+      this.averageMax = (this.averageMax/7).toFixed(2);
     }
   }
 };
+
+
+
+
 </script>
