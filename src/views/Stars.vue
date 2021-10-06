78265d74e109102739186d4c5e6447e590ed1ee0
Nick Wang <hwan0109@student.monash.edu>
2021-10-06 14:13:55 +1100

    fetch weather for calcluating visibility

--- src/views/Stars.vue
+++ src/views/Stars.vue
@@ -4,18 +4,20 @@
     <h2>Stars</h2>
   </div>
   <div class="card-body" style="position: relative;">
-    <StarWatch @removed="removeStar" :stars="stars" />
-    <StarsAvailable @added="addStar" />
+    <StarWatch @removed="removeStar" :stars="stars" :weather="weather" />
+    <StarsAvailable @added="addStar" :weather="weather" />
   </div>
 
 </template>
 
 <script>
+import axios from "axios";
 
 import StarWatch from "@/components/StarWatch";
 import StarsAvailable from "@/components/StarsAvailable";
 
 import { starName } from "../util/star";
+import { currentDate } from "../util/date";
 
 const STAR_WATCH_KEY = "star-watch";
 
@@ -30,15 +32,26 @@ export default {
   },
 
   data() {
+    // Retrieve stars from storage
     const stars = JSON.parse(window.localStorage.getItem(STAR_WATCH_KEY) ?? "[]");
 
     return {
-      stars
+      stars,
+      weather: null
     };
   },
+
+  async created() {
+    // Weather at the current time
+    const res = await axios.get(`http://api.weatherapi.com/v1/history.json?key=721ef4891d454f2385304513211009&q=-37.91063,145.13360699999998&dt=` + currentDate());
+    const hourWeather = res.data.forecast.forecastday[0].hour;
+
+    this.weather = hourWeather.find(({ time_epoch }) => Math.abs(time_epoch - Date.now()) < 3600);
+  },
+
   methods: {
     addStar(star) {
-      console.log("add", starName(star), this.stars);
+      console.log(star, this.stars);
 
       // Don't add duplicates
       if (!this.stars.find(({ name }) => name === starName(star))) {
